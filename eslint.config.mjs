c6415a7131b619251b67f0e432e49eb93970b9d0
Julianne Adams <julianne@julianneadams.dev>
2024-12-16 16:31:59 -0500

    add: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+// @ts-check
+
+import prettierConfig from "eslint-config-prettier";
+
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  prettierConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: {
+          allowDefaultProject: ["*.mjs"],
+        },
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        { allowNumber: true },
+      ],
+    },
+  },
+);
