72badc30ddca34514caa1549388a549389ce4dbd
Julianne Adams <julianne@julianneadams.dev>
2024-12-18 15:53:24 -0500

    add: logic for adding new posts to database

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,4 +27,12 @@ export default tseslint.config(
       ],
     },
   },
+  {
+    files: ["src/posts.ts"],
+    rules: {
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+    },
+  },
 );
--- src/posts.ts
+++ src/posts.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import db from "./db";
+import { postsTable } from "./db/schema";
 
 const app = new Hono();
 
@@ -14,7 +16,50 @@ app.get("/:id", (c) => {
   return c.text(`This route returns the blog post with ID ${id}.`);
 });
 
-app.post("/", (c) => c.text("This route creates a new post."));
+// TODO: add request validation
+// TODO: add ability to include tags in request
+app.post("/", async (c) => {
+  const {
+    title,
+    content,
+    published,
+    publicationDate,
+  }: {
+    title: string;
+    content: string;
+    published: boolean;
+    publicationDate: string;
+  } = await c.req.json();
+
+  const postValues = {
+    title,
+    content,
+    published,
+    publicationDate,
+  };
+
+  try {
+    await db.insert(postsTable).values(postValues);
+  } catch (err: unknown) {
+    const message = err instanceof Error ? err.message : "Error creating post.";
+
+    return c.json(
+      {
+        ok: false,
+        message,
+      },
+      500,
+    );
+  }
+
+  return c.json(
+    {
+      ok: true,
+      message: "Post created.",
+    },
+    201,
+  );
+});
 
 app.patch("/:id", (c) => c.text(`Post ${c.req.param("id")} updated!`));
 
