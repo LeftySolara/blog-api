13910d6b354b8f3372f81e7ce6333770e25be112
Julianne Adams <julianne@julianneadams.dev>
2024-12-18 16:53:31 -0500

    add: request validation with zod

--- src/posts.ts
+++ src/posts.ts
@@ -1,6 +1,17 @@
 import { Hono } from "hono";
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
 import db from "./db";
 import { postsTable } from "./db/schema";
+
+const CreatePostRequestSchema = z
+  .object({
+    title: z.string(),
+    content: z.string(),
+    published: z.boolean(),
+    publicationDate: z.string().date(),
+  })
+  .partial({ publicationDate: true });
 
 const app = new Hono();
 
@@ -16,50 +27,47 @@ app.get("/:id", (c) => {
   return c.text(`This route returns the blog post with ID ${id}.`);
 });
 
-// TODO: add request validation
 // TODO: add ability to include tags in request
-app.post("/", async (c) => {
-  const {
-    title,
-    content,
-    published,
-    publicationDate,
-  }: {
-    title: string;
-    content: string;
-    published: boolean;
-    publicationDate: string;
-  } = await c.req.json();
+app.post(
+  "/",
+  zValidator("json", CreatePostRequestSchema, (result, c) => {
+    if (!result.success) {
+      return c.json(
+        {
+          ok: false,
+          message: `${result.error.issues[0]["path"][0]}: ${result.error.issues[0]["message"]}`,
+        },
+        400,
+      );
+    }
+  }),
+  async (c) => {
+    const reqBody = await c.req.json();
 
-  const postValues = {
-    title,
-    content,
-    published,
-    publicationDate,
-  };
+    try {
+      await db.insert(postsTable).values(reqBody);
+    } catch (err: unknown) {
+      const message =
+        err instanceof Error ? err.message : "Error creating post.";
 
-  try {
-    await db.insert(postsTable).values(postValues);
-  } catch (err: unknown) {
-    const message = err instanceof Error ? err.message : "Error creating post.";
+      return c.json(
+        {
+          ok: false,
+          message,
+        },
+        500,
+      );
+    }
 
     return c.json(
       {
-        ok: false,
-        message,
+        ok: true,
+        message: "Post created.",
       },
-      500,
+      201,
     );
-  }
-
-  return c.json(
-    {
-      ok: true,
-      message: "Post created.",
-    },
-    201,
-  );
-});
+  },
+);
 
 app.patch("/:id", (c) => c.text(`Post ${c.req.param("id")} updated!`));
 
