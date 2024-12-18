12ccb241af9ddafe6f1fd27ec28c358b522b0040
Julianne Adams <julianne@julianneadams.dev>
2024-12-18 12:57:34 -0500

    refactor: un-chain posts routes

--- src/posts.ts
+++ src/posts.ts
@@ -1,18 +1,23 @@
 import { Hono } from "hono";
 
-const app = new Hono()
-  .get("/", (c) => {
-    return c.json({
-      ok: true,
-      message: "This route returns a list of blog posts.",
-    });
-  })
-  .get("/:id", (c) => {
-    const id = c.req.param("id");
-    return c.text(`This route returns the blog post with ID ${id}.`);
-  })
-  .post("/", (c) => c.text("This route creates a new post."))
-  .patch("/:id", (c) => c.text(`Post ${c.req.param("id")} updated!`))
-  .delete("/:id", (c) => c.text(`Post ${c.req.param("id")} deleted!`));
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.json({
+    ok: true,
+    message: "This route returns a list of blog posts.",
+  });
+});
+
+app.get("/:id", (c) => {
+  const id = c.req.param("id");
+  return c.text(`This route returns the blog post with ID ${id}.`);
+});
+
+app.post("/", (c) => c.text("This route creates a new post."));
+
+app.patch("/:id", (c) => c.text(`Post ${c.req.param("id")} updated!`));
+
+app.delete("/:id", (c) => c.text(`Post ${c.req.param("id")} deleted!`));
 
 export default app;
